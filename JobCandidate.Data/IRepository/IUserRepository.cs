17aaf575e39cda91ae0a3f99ff768834b1cf0680
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-04-22 22:18:56 +0500

    implemented UserRepository, added IUserRepository #3

---
+++ JobCandidate.Data/IRepository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobCandidate.Domain.Entities;
+
+namespace JobCandidate.Data.IRepository
+{
+    internal interface IUserRepository
+    {
+        public Task<User> CreateAsync(User user);
+        public Task<User> UpdateAsync(User user);
+        public Task<User> SelectByIdAsync(int id);
+        public IQueryable<User> SelectAll();
+        public Task<bool> DeleteByIdAsync(int id);
+    }
+}
