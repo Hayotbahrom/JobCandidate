f712a2c539faa2f6eddbff39697d51d7c2e5a924
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-04-23 18:29:31 +0500

    Added CustomException

---
+++ JobCandidate.Service/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+﻿namespace JobCandidate.Service.Exceptions
+{
+    public class CustomException : Exception
+    {
+        public int statusCode;
+        public CustomException(int code, string message) : base(message)
+        {
+            statusCode = code;
+        }
+    }
+}
--- JobCandidate.Service/Services/UserService.cs
+++ JobCandidate.Service/Services/UserService.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JobCandidate.Data.IRepository;
+using JobCandidate.Domain.Entities;
+using JobCandidate.Service.Interfaces;
 
 namespace JobCandidate.Service.Services
 {
-    public class UserService
+    public class UserService : IIUserService
     {
+        private readonly IUserRepository userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public Task<User> CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
