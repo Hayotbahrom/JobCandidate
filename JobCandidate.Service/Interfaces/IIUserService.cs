595d0c7edd13513b21fcc98db7baecc2f134f37b
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-04-23 18:25:16 +0500

    added IUserService interface

---
+++ JobCandidate.Service/Interfaces/IIUserService.cs
@@ -0,0 +1,18 @@
+﻿using JobCandidate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobCandidate.Service.Interfaces
+{
+    public interface IIUserService
+    {
+        Task<User> CreateAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<User> DeleteAsync(int id);
+        Task<bool> GetByIdAsync(int id);
+        Task<IEnumerable<User>> GetAllAsync();
+    }
+}
