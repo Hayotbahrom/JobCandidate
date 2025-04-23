2d49ce3612dff09867aef5396165c293aa313312
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2025-04-23 18:43:00 +0500

    Added UserDto and AddOrUpdateUser method

--- JobCandidate.Service/Interfaces/IIUserService.cs
+++ JobCandidate.Service/Interfaces/IIUserService.cs
@@ -1,4 +1,5 @@
 ﻿using JobCandidate.Domain.Entities;
+using JobCandidate.Service.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,9 @@ namespace JobCandidate.Service.Interfaces
 {
     public interface IIUserService
     {
-        Task<User> CreateAsync(User user);
-        Task<User> UpdateAsync(User user);
+        Task<User> AddOrUpdateUserAsync(UserDto userDto);
+        Task<User> CreateAsync(UserDto userDto);
+        Task<User> UpdateAsync(UserDto userDto);
         Task<User> DeleteAsync(int id);
         Task<bool> GetByIdAsync(int id);
         Task<IEnumerable<User>> GetAllAsync();
